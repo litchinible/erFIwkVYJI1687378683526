e845d1a37952a9b8ef1b77597b7506bfa12b8208
EasyApk <easyapk@icloud.com>
2023-06-21 22:38:37 +0000

    update

--- Assets/MaxSdk/Scripts/Editor/MaxPostProcessBuildAndroid.cs
+++ Assets/MaxSdk/Scripts/Editor/MaxPostProcessBuildAndroid.cs
@@ -25,7 +25,7 @@ namespace AppLovinMax.Scripts.Editor
     /// </summary>
     public class MaxPostProcessBuildAndroid : IPostGenerateGradleAndroidProject
     {
-        private static string APPLOVIN_SDK_KEY = "KkogXiRi8yVcf7gMTqQPLAp7lCJZE1p_a59ieHT8JP-c0nDK7dmSUw_Ne3vtzl8TU4ipw-vg_aIdT2UlLl9kcP";
+        private static string APPLOVIN_SDK_KEY = "dy81XL4M1BWEhczSGTr6nXvAyJu4MZR1HdfMNT8xEsE1xbxioZdkOTqzb0tLuxoVDE2bqSwNSacqZdFPIkbxTa";
 #if UNITY_2019_3_OR_NEWER
         private const string PropertyAndroidX = "android.useAndroidX";
         private const string PropertyJetifier = "android.enableJetifier";
